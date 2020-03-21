c08e7ee2b1b8fb048da7daaa865825c89bddf7fc
Josué Camelo dos Santos Ferreira <josueprg@gmai.com>
2020-03-21 18:11:35 -0300

    Configuração Realm Definida na Classe Principal MyApplication

--- app/src/main/java/com/example/projetorealm/MyApplication.java
+++ app/src/main/java/com/example/projetorealm/MyApplication.java
@@ -2,9 +2,21 @@ package com.example.projetorealm;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration
+                .Builder()
+                .name("meuprimeirobanco.realm").build();
+
+        //Toda Vez vai Apagar os Dados do Banco, Mod DEV, remover em produção
+        Realm.deleteRealm(config);
+        Realm.setDefaultConfiguration(config);
     }
 }
